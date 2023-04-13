fb11ce6e79924edd53bab5f158f3f09f2a016bfa
ashebani <azizoo2001.as@gmail.com>
2023-04-13 02:25:56 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import dice from "./images/icon-dice.svg";
+import patternMobile from "./images/pattern-divider-mobile.svg";
+import patternDesktop from "./images/pattern-divider-desktop.svg";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [wisdom, setWisdom] = useState({
+    slip: { id: 15, advice: "If it ain't broke don't fix it." },
+  });
+  const wisdomAPI = () => {
+    fetch(`https://api.adviceslip.com/advice/${getRandomInt(100)}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setWisdom(data);
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    wisdomAPI();
+  }, []);
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <p className="advice">Advice #{wisdom.slip.id}</p>
+      <p className="wisdom">{`"${wisdom.slip.advice}"`}</p>
+      <div className="pattern">
+        <picture>
+          <source media="(min-width:500px)" srcSet={patternDesktop} />
+          <img src={patternMobile} alt="Flowers" />
+        </picture>
+      </div>
+
+      <div className="dice" onClick={wisdomAPI}>
+        <img src={dice} alt="" />
+      </div>
+    </main>
   );
 }
 
